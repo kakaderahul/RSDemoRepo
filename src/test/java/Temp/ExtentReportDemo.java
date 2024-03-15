773da83217ac4fbbd64d66bc016dbdaa09a46412
rahul <kakaderahul70@gmail.com>
2024-03-15 09:21:43 +0530

    first commit

---
+++ src/test/java/Temp/ExtentReportDemo.java
@@ -0,0 +1,21 @@
+package Temp;
+
+import org.testng.annotations.Test;
+
+public class ExtentReportDemo {
+	
+	
+	
+	
+	
+	@Test
+	public void getTitleTest() {
+		
+	WebDriverManager.chromeDriver.setup();
+		
+		
+	}
+	
+	
+
+}
